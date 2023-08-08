39cc8baef8dc76f0c1a55c5761482b5e6a51c858
M- Yusuf <yusufmalik0075@gmail.com>
2023-08-08 06:35:48 +0500

    Updated errors

--- config/settings.py
+++ config/settings.py
@@ -206,6 +206,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "fettlerplus@gmail.com"
 EMAIL_HOST_PASSWORD = "iqqhuzvybeqdpdrk"
-# EMAIL_HOST_PASSWORD = "Fettler++123"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
