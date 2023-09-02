4379be82fe68001bee647d013c8549ee4d669a87
Muhammad Yousaf Malik <89022826+myousafmalik@users.noreply.github.com>
2023-09-02 19:41:11 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -205,6 +205,6 @@ APPEND_SLASH = False
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "fettlerplus@gmail.com"
-EMAIL_HOST_PASSWORD = "iqqhuzvybeqdpdrk"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
